f285e54c1525cdbf6f0240ad02d3800e1d83073a
fabio-cumbo <fabio.cumbo@gmail.com>
2023-04-25 23:48:24 -0400

    Add new arguments
    --report-counts, --top-perc, and --top-num

--- minimizers/minimizers.py
+++ minimizers/minimizers.py
@@ -4,7 +4,7 @@ Extract the set of minimizers from a sequence file
 """
 
 __author__ = "Fabio Cumbo (fabio.cumbo@gmail.com)"
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __date__ = "Apr 25, 2023"
 
 import argparse as ap
@@ -13,8 +13,10 @@ import gzip
 import multiprocessing as mp
 import os
 import time
+from collections import Counter
+from collections.abc import Callable
 from functools import partial
-from typing import Dict, Set
+from typing import Dict, Optional, Set, Union
 
 from Bio import SeqIO
 
@@ -65,7 +67,7 @@ def read_params():
     p.add_argument(
         "-s",
         "--size",
-        type=int,
+        type=number(int, minv=4),
         required=True,
         dest="size",
         help="Length of the minimizers",
@@ -73,7 +75,7 @@ def read_params():
     p.add_argument(
         "-w",
         "--window",
-        type=int,
+        type=number(int, minv=5),
         required=True,
         dest="window",
         help=(
@@ -82,9 +84,31 @@ def read_params():
         ),
     )
     p.add_argument(
+        "--report-counts",
+        action="store_true",
+        default=False,
+        dest="report_counts",
+        help=(
+            "Report the frequencies of the minimizers. "
+            "This is compatible with \"--output-type list\" only"
+        )
+    )
+    p.add_argument(
+        "--top-perc",
+        type=number(float, minv=1.0, maxv=100.0),
+        dest="top_perc",
+        help="Report the top percentage of minimizers based on their frequency",
+    )
+    p.add_argument(
+        "--top-num",
+        type=number(int, minv=1),
+        dest="top_num",
+        help="Report the top number of minimizers based on their frequency",
+    )
+    p.add_argument(
         "-n",
         "--nproc",
-        type=int,
+        type=number(int, minv=1, maxv=os.cpu_count()),
         default=1,
         dest="nproc",
         help="Make it parallel",
@@ -103,6 +127,37 @@ def read_params():
         help='Print the "{}" version and exit'.format(TOOL_ID),
     )
     return p.parse_args()
+
+
+def number(
+    typev: type,
+    minv: Optional[Union[int, float]] = None,
+    maxv: Optional[Union[int, float]] = None,
+) -> Callable:
+    """
+    Take full control of input numeric types by defining custom intervals
+    """
+
+    def type_func(value: Union[int, float]) -> Union[int, float]:
+        """
+        Test data type and ranges on the input value
+        """
+
+        try:
+            value = typev(value)
+
+            if minv and value < minv:
+                raise ap.ArgumentTypeError("Minimum value is {}".format(minv))
+
+            if maxv and value > maxv:
+                raise ap.ArgumentTypeError("Maximum value is {}".format(maxv))
+
+            return value
+
+        except Exception as e:
+            raise ap.ArgumentTypeError("Input value must be {}".format(typev)).with_traceback(e.__traceback__)
+
+    return type_func
 
 
 def openrt(filepath: str, mode: str = "r"):
@@ -137,7 +192,7 @@ def get_minimizers(sequence_id: str, sequence_content: str, kmer_size: int = 4, 
     :param sequence_content:    The actual sequence
     :param kmer_size:           Size of the minimizers
     :param window_size:         Size of the sliding window
-    :return:                    A dictionary with just the set of minimizers indexed by the sequence ID
+    :return:                    A dictionary with just the set of minimizers and their frequencies indexed by the sequence ID
     """
 
     # Extract kmers
@@ -146,8 +201,8 @@ def get_minimizers(sequence_id: str, sequence_content: str, kmer_size: int = 4, 
     # Define windows
     windows = [kmers[i:i + window_size] for i in range(0, len(kmers) - window_size + 1, 1)]
 
-    # Get minimizers
-    minimizers = set([min(window) for window in windows])
+    # Get minimizers and their frequencies
+    minimizers = Counter([min(window) for window in windows])
 
     return {sequence_id: minimizers}
 
@@ -165,7 +220,7 @@ def get_minimizers_parallel(
     :param kmer_size:   Size of the minimizers
     :param window_size: Size of the sliding window
     :param nproc:       Number of parallel processes
-    :return:            A dictionary with the set of minimizers indexed by the sequence IDs
+    :return:            A dictionary with the set of minimizers and their frequencies indexed by the sequence IDs
     """
 
     minimizers = dict()
@@ -188,7 +243,7 @@ def get_minimizers_parallel(
 
         for job in jobs:
             job.wait()
-    
+
     return minimizers
 
 
@@ -205,6 +260,9 @@ def main() -> None:
 
     if not os.path.isdir(outfolder):
         raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), outfolder)
+
+    if args.output_type == "fasta" and args.report_counts:
+        raise ValueError("\"--report-counts\" is not compatible with \"--output-type fasta\"")
 
     t0 = time.time()
 
@@ -227,38 +285,84 @@ def main() -> None:
     )
 
     if args.aggregate:
-        minimizers = set().union(*minimizers.values())
+        merged = None
+        
+        for record in minimizers:
+            if not merged:
+                merged = minimizers[record]
+            
+            else:
+                merged.update(minimizers[record])
+
+        minimizers = merged
 
         if args.verbose:
             print("The input file contains {} minimizers".format(len(minimizers)))
 
     if args.verbose:
         print("Dumping results to {}".format(args.output))
-    
+
     with open(args.output, "w+") as output:
-        if isinstance(minimizers, set):
+        if isinstance(minimizers, Counter):
+            sorted_minimizers = minimizers.keys()
+
+            if args.top_num or args.top_perc:
+                sorted_minimizers = sorted(sorted_minimizers, key=lambda mini: minimizers[mini], reverse=True)
+
+            mini_counter = 0
+
             if args.output_type == "fasta":
                 output.write(">{}\n".format(os.path.basename(args.input)))
 
-            for mini in minimizers:
+            for mini in sorted_minimizers:
                 output.write(
-                    "{}{}\n".format(
+                    "{}{}{}\n".format(
                         "N" if args.output_type == "fasta" else "",
-                        mini
+                        mini,
+                        "\t{}".format(minimizers[mini]) if args.output_type == "list" and args.report_counts else ""
                     )
                 )
-        
+
+                mini_counter += 1
+
+                if args.top_num and mini_counter >= args.top_num:
+                    break
+
+                elif args.top_perc and (mini_counter / len(minimizers) * 100.0) >= args.top_perc:
+                    break
+
         else:
             for record in minimizers:
+                sorted_minimizers = minimizers[record].keys()
+
+                if args.top_num or args.top_perc:
+                    sorted_minimizers = sorted(sorted_minimizers, key=lambda mini: minimizers[record][mini], reverse=True)
+
+                mini_counter = 0
+
                 if args.output_type == "fasta":
                     output.write(">{}\n".format(record))
 
-                for mini in minimizers[record]:
+                for mini in sorted_minimizers:
                     if args.output_type == "fasta":
                         output.write("N{}\n".format(mini))
 
                     else:
-                        output.write("{}\t{}\n".format(record, mini))
+                        output.write(
+                            "{}\t{}{}\n".format(
+                                record,
+                                mini,
+                                "\t{}".format(minimizers[record][mini]) if args.report_counts else ""
+                            )
+                        )
+
+                    mini_counter += 1
+
+                    if args.top_num and mini_counter >= args.top_num:
+                        break
+
+                    elif args.top_perc and (mini_counter / len(minimizers) * 100.0) >= args.top_perc:
+                        break
 
     t1 = time.time()
 
